2e551966fac18d4d26441484ae9b95546f2ff92e
chamseddine <chamseddine.benhamed@ensi-uma.tn>
2021-08-12 17:11:03 +0200

    review change
    Signed-off-by: chamseddine <chamseddine.benhamed@ensi-uma.tn>

--- src/main/java/org/gridsuite/directory/server/DirectoryService.java
+++ src/main/java/org/gridsuite/directory/server/DirectoryService.java
@@ -213,7 +213,7 @@ class DirectoryService {
             directoryElementRepository.updateElementAccessRights(elementUuid, newIsPrivate);
             directoryElementRepository.findById(elementUuid).ifPresent(directoryElementEntity -> {
                 boolean isPrivate = isPrivateForNotification(directoryElementEntity.getParentId(), false);
-                // second parameter should be always false because when we pass a root folder from public -> private we should notify all clients                emitDirectoryChanged(
+                // second parameter should be always false because when we pass a root folder from public -> private we should notify all clients
                 emitDirectoryChanged(
                         directoryElementEntity.getParentId() == null ? directoryElementEntity.getId() : directoryElementEntity.getParentId(),
                         userId,
