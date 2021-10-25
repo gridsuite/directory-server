f4a1400adcdcf5ce62c8467ec0b2c7599a168a9d
HOMER Etienne <etiennehomer@gmail.com>
2021-10-25 09:27:38 +0200

    clean imports
    Signed-off-by: HOMER Etienne <etiennehomer@gmail.com>

--- src/main/java/org/gridsuite/directory/server/DirectoryApplication.java
+++ src/main/java/org/gridsuite/directory/server/DirectoryApplication.java
@@ -6,8 +6,6 @@
  */
 package org.gridsuite.directory.server;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.config.EnableWebFlux;
