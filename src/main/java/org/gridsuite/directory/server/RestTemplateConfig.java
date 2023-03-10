c14e9ff4fdf710f47cef75db70881d3bdd0ddf65
sBouzols <sylvain.bouzols@gmail.com>
2023-03-10 17:57:09 +0100

    do not change objectMapper config
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/main/java/org/gridsuite/directory/server/RestTemplateConfig.java
+++ src/main/java/org/gridsuite/directory/server/RestTemplateConfig.java
@@ -8,13 +8,6 @@ package org.gridsuite.directory.server;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -52,16 +45,6 @@ public class RestTemplateConfig {
     public static ObjectMapper createObjectMapper() {
         var objectMapper = Jackson2ObjectMapperBuilder.json().build();
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-
-        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
-
-        LocalDateTimeDeserializer dateTimeDeserializer = new LocalDateTimeDeserializer(formatter);
-        LocalDateTimeSerializer dateTimeSerializer = new LocalDateTimeSerializer(formatter);
-
-        JavaTimeModule javaTimeModule = new JavaTimeModule();
-        javaTimeModule.addDeserializer(LocalDateTime.class, dateTimeDeserializer);
-        javaTimeModule.addSerializer(LocalDateTime.class, dateTimeSerializer);
-        objectMapper.registerModule(javaTimeModule);
         return objectMapper;
     }
 
