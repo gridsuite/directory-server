f777ab23ca3b92ca20e511fc88fd469e1c5a8166
Lilian Houdelet <lhoudelet@oxyl.fr>
2024-01-09 16:38:13 +0100

    Make a different call when more elements are needed and when only directories are needed
    Signed-off-by: Lilian Houdelet <lhoudelet@oxyl.fr>

--- src/main/java/org/gridsuite/directory/server/DirectoryService.java
+++ src/main/java/org/gridsuite/directory/server/DirectoryService.java
@@ -263,7 +263,12 @@ public class DirectoryService {
 
     public List<ElementAttributes> getRootDirectories(String userId, List<String> types) {
         List<DirectoryElementEntity> directoryElements = directoryElementRepository.findRootDirectoriesByUserId(userId);
-        Map<UUID, Long> subdirectoriesCountsMap = getSubElementsCount(directoryElements.stream().map(DirectoryElementEntity::getId).collect(Collectors.toList()), types, userId);
+        Map<UUID, Long> subdirectoriesCountsMap;
+        if (!types.isEmpty()) {
+            subdirectoriesCountsMap = getSubElementsCount(directoryElements.stream().map(DirectoryElementEntity::getId).collect(Collectors.toList()), types);
+        } else {
+            subdirectoriesCountsMap = getSubElementsCount(directoryElements.stream().map(DirectoryElementEntity::getId).collect(Collectors.toList()), types, userId);
+        }
         return directoryElements.stream()
                 .map(e -> toElementAttributes(e, subdirectoriesCountsMap.getOrDefault(e.getId(), 0L)))
                 .collect(Collectors.toList());
