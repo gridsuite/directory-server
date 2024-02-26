b255c28f74ed1280a25be16627dcac06ac0265e1
Rehili Ghazwa <ghazwarhili@gmail.com>
2024-02-26 17:59:27 +0100

    add test for reindex all endpoint

---
+++ src/test/java/org/gridsuite/directory/server/elasticsearch/DirectoryElementInfosControllerTest.java
@@ -0,0 +1,87 @@
+/*
+  Copyright (c) 2024, RTE (http://www.rte-france.com)
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.gridsuite.directory.server.elasticsearch;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Iterables;
+import org.gridsuite.directory.server.dto.ElementAttributes;
+import org.gridsuite.directory.server.repository.DirectoryElementEntity;
+import org.gridsuite.directory.server.repository.DirectoryElementRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author Ghazwa Rehili <ghazwa.rehili at rte-france.com>
+ */
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+@SpringBootTest
+class DirectoryElementInfosControllerTest {
+
+    @Autowired
+    DirectoryElementInfosService directoryElementInfosService;
+
+    @Autowired
+    DirectoryElementInfosRepository directoryElementInfosRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    private DirectoryElementRepository directoryElementRepository;
+
+    private void cleanDB() {
+        directoryElementInfosRepository.deleteAll();
+    }
+
+    @BeforeEach
+    public void setup() {
+        cleanDB();
+    }
+
+    @Test
+    void testReindexAllElementsEndpoint() throws Exception {
+        String userId = "user";
+        ZonedDateTime now = ZonedDateTime.now(ZoneOffset.UTC).truncatedTo(ChronoUnit.MICROS);
+        ElementAttributes caseElement = ElementAttributes.toElementAttributes(UUID.randomUUID(), "caseName", "CASE",
+                false, "user", null, now, now, userId);
+        String requestBody = objectMapper.writeValueAsString(caseElement);
+        mockMvc.perform(post("/v1/directories/paths/elements?directoryPath=" + "dir")
+                        .header("userId", userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk());
+
+        List<DirectoryElementEntity> directoryElementList = directoryElementRepository.findAll();
+        assertEquals(2, directoryElementList.size());
+
+        mockMvc.perform(post("/v1/elements/reindex-all"))
+                .andExpect(status().isOk());
+
+        assertEquals(2, Iterables.size(directoryElementInfosRepository.findAll()));
+    }
+}
