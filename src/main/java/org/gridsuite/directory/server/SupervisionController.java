b8d999a0a5b2dfdb65c9569bd543e439d2093626
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2024-04-15 16:44:26 +0200

    feat: add tests + add copyrights
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/main/java/org/gridsuite/directory/server/SupervisionController.java
+++ src/main/java/org/gridsuite/directory/server/SupervisionController.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.gridsuite.directory.server;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,6 +18,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * @author Kevin Le Saulnier <kevin.lesaulnier at rte-france.com>
+ */
 @RestController
 @RequestMapping(value = "/" + DirectoryApi.API_VERSION + "/supervision")
 @Tag(name = "directory-server - Supervision")
