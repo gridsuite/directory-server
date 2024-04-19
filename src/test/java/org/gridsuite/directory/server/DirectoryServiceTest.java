9832f90ef9791f67277db8439c575cadd9e97c0d
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2024-04-19 14:19:54 +0200

    fix: checkstyle
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/test/java/org/gridsuite/directory/server/DirectoryServiceTest.java
+++ src/test/java/org/gridsuite/directory/server/DirectoryServiceTest.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
--- src/test/java/org/gridsuite/directory/server/utils/DirectoryTestUtils.java
+++ src/test/java/org/gridsuite/directory/server/utils/DirectoryTestUtils.java
@@ -5,7 +5,11 @@ import org.gridsuite.directory.server.repository.DirectoryElementEntity;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public class DirectoryTestUtils {
+public final class DirectoryTestUtils {
+    private DirectoryTestUtils() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
     public static DirectoryElementEntity createRootElement(String elementName, String type, boolean isPrivate, String userId) {
         return new DirectoryElementEntity(UUID.randomUUID(), null, elementName, type, isPrivate, userId, null, LocalDateTime.now(), LocalDateTime.now(), userId, false, null);
     }
