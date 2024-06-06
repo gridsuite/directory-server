3c8eaf53c200895080a3fc1c07ba63497ae41417
Tristan Chuine <tristan.chuine_externe@rte-france.com>
2024-06-06 18:34:16 +0200

    Add endpoint to recreate the index
    Now free from manual `curl` commands!

--- src/main/java/org/gridsuite/directory/server/services/SupervisionService.java
+++ src/main/java/org/gridsuite/directory/server/services/SupervisionService.java
@@ -1,8 +1,12 @@
 package org.gridsuite.directory.server.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.gridsuite.directory.server.dto.ElementAttributes;
+import org.gridsuite.directory.server.dto.elasticsearch.DirectoryElementInfos;
 import org.gridsuite.directory.server.repository.DirectoryElementEntity;
 import org.gridsuite.directory.server.repository.DirectoryElementRepository;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.IndexOperations;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,13 +15,17 @@ import java.util.UUID;
 import static org.gridsuite.directory.server.dto.ElementAttributes.toElementAttributes;
 
 @Service
+@Slf4j
 public class SupervisionService {
     private final DirectoryElementRepository directoryElementRepository;
     private final DirectoryRepositoryService repositoryService;
+    private final ElasticsearchTemplate esTemplate;
 
-    public SupervisionService(DirectoryRepositoryService repositoryService, DirectoryElementRepository directoryElementRepository) {
+    public SupervisionService(DirectoryRepositoryService repositoryService, DirectoryElementRepository directoryElementRepository,
+                              ElasticsearchTemplate elasticsearchTemplate) {
         this.repositoryService = repositoryService;
         this.directoryElementRepository = directoryElementRepository;
+        this.esTemplate = elasticsearchTemplate;
     }
 
     public List<ElementAttributes> getStashedElementsAttributes() {
@@ -34,5 +42,29 @@ public class SupervisionService {
 
     public List<DirectoryElementEntity> getStashedElements() {
         return directoryElementRepository.findAllByStashed(true);
+    }
+
+    public boolean recreateIndexDirectoryElementInfos() {
+        final IndexOperations idxDirectoryElementInfos = esTemplate.indexOps(DirectoryElementInfos.class);
+        final String idxDirectoryElementInfosName = idxDirectoryElementInfos.getIndexCoordinates().getIndexName();
+        log.warn("Recreating ElasticSearch index {}", idxDirectoryElementInfosName);
+        if (idxDirectoryElementInfos.exists()) {
+            log.info("Index {} found, delete it.", idxDirectoryElementInfosName);
+            if (idxDirectoryElementInfos.delete()) {
+                log.info("Successfully delete index {}", idxDirectoryElementInfosName);
+            } else {
+                log.error("A problem seems to happen when deleting index {}...", idxDirectoryElementInfosName);
+                return false;
+            }
+        }
+        if (idxDirectoryElementInfos.createWithMapping()) {
+            log.info("Index {} successfully recreated!", idxDirectoryElementInfosName);
+        } else {
+            log.info("An error happen while re-creating index {}...", idxDirectoryElementInfosName);
+            return false;
+        }
+        log.info("Re-indexing all elements of {}", idxDirectoryElementInfosName);
+        repositoryService.reindexAllElements();
+        return true;
     }
 }
