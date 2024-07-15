c19e536672de776b4d80a19a4996dcc6ce6fea21
jamal-khey <myjamal89@gmail.com>
2024-07-15 13:22:09 +0200

    fix styles

--- src/main/java/org/gridsuite/directory/server/elasticsearch/ESUtils.java
+++ src/main/java/org/gridsuite/directory/server/elasticsearch/ESUtils.java
@@ -9,6 +9,12 @@ import org.springframework.data.elasticsearch.core.SearchHits;
 import java.util.List;
 
 public final class ESUtils {
+
+    private ESUtils() {
+        // This constructor is private to prevent instantiation of the utility class.
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
     public static <T> Page<T> searchHitsToPage(SearchHits<T> searchHits, Pageable pageable) {
         List<T> content = searchHits.stream().map(SearchHit::getContent).toList();
         return new PageImpl<>(content, pageable, searchHits.getTotalHits());
