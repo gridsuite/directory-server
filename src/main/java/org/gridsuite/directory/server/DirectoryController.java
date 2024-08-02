7510c5f27152f26470a0b1e4b21653aad73b5b9b
Franck LECUYER <franck.lecuyer@rte-france.com>
2024-08-02 10:08:53 +0200

    Fix compilation pb
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/main/java/org/gridsuite/directory/server/DirectoryController.java
+++ src/main/java/org/gridsuite/directory/server/DirectoryController.java
@@ -21,6 +21,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.UUID;
 
