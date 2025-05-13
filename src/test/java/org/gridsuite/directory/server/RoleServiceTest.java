babf87221ee5ad5590da68f9f40de8130015bdbd
achour94 <berrahmaachour@gmail.com>
2025-05-13 13:09:44 +0200

    Check user roles from request header
    Signed-off-by: achour94 <berrahmaachour@gmail.com>

---
+++ src/test/java/org/gridsuite/directory/server/RoleServiceTest.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.gridsuite.directory.server;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.gridsuite.directory.server.constants.ApplicationRoles;
+import org.gridsuite.directory.server.services.RoleService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.lang.reflect.Constructor;
+import java.util.Set;
+
+import static org.gridsuite.directory.server.services.RoleService.ROLES_HEADER;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Achour Berrahma <achour.berrahma at rte-france.com>
+ */
+@SpringBootTest(classes = {DirectoryApplication.class})
+class RoleServiceTest {
+
+    @InjectMocks
+    private RoleService roleService;
+
+    @Mock
+    private HttpServletRequest request;
+
+    @Mock
+    private ServletRequestAttributes attributes;
+
+    @BeforeEach
+    void setUp() {
+        // Set up the RequestContextHolder with our mocked ServletRequestAttributes
+        when(attributes.getRequest()).thenReturn(request);
+        RequestContextHolder.setRequestAttributes(attributes);
+    }
+
+    @AfterEach
+    void tearDown() {
+        // Clear the RequestContextHolder after each test
+        RequestContextHolder.resetRequestAttributes();
+    }
+
+    @Test
+    void testHasRequiredRolesWithNoMatchingRolesAllRequired() {
+        // Test when the user has none of the required roles (all required)
+        when(request.getHeader(ROLES_HEADER)).thenReturn("VIEWER|GUEST");
+
+        boolean hasRoles = roleService.hasRequiredRoles(Set.of("ADMIN", "USER"), true);
+
+        assertFalse(hasRoles);
+        verify(request).getHeader(ROLES_HEADER);
+    }
+
+    @Test
+    void testCheckAccessWhenUserHasRequiredRoles() {
+        when(request.getHeader(ROLES_HEADER)).thenReturn("ADMIN_EXPLORE|USER");
+        assertTrue(roleService.isUserExploreAdmin());
+    }
+
+    @Test
+    void testCheckAccessWhenUserDoesNotHaveRequiredRoles() {
+        when(request.getHeader(ROLES_HEADER)).thenReturn("USER|ADMIN");
+        assertFalse(roleService.isUserExploreAdmin());
+        verify(request).getHeader(ROLES_HEADER);
+    }
+
+    @Test
+    void testApplicationRolesPrivateConstructor() {
+        // Testing that private constructor throws AssertionError when called (to add test coverage)
+        assertThrows(Exception.class, () -> {
+            // Using reflection to access and invoke the private constructor
+            Constructor<ApplicationRoles> constructor = ApplicationRoles.class.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            constructor.newInstance();
+        });
+    }
+
+}
