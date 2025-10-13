981e9e99646741a61338433944b854d56332cdbd
benrejebmoh <mohamed.ben-rejeb@rte-france.com>
2025-10-13 14:51:15 +0200

    fix license header
    Signed-off-by: benrejebmoh <mohamed.ben-rejeb@rte-france.com>

--- src/test/java/org/gridsuite/directory/server/DirectoryBusinessErrorCodeTest.java
+++ src/test/java/org/gridsuite/directory/server/DirectoryBusinessErrorCodeTest.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.gridsuite.directory.server;
 
 import org.junit.jupiter.params.ParameterizedTest;
@@ -5,6 +11,9 @@ import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * @author Mohamed Ben-rejeb {@literal <mohamed.ben-rejeb at rte-france.com>}
+ */
 class DirectoryBusinessErrorCodeTest {
 
     @ParameterizedTest
--- src/test/java/org/gridsuite/directory/server/DirectoryExceptionTest.java
+++ src/test/java/org/gridsuite/directory/server/DirectoryExceptionTest.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.gridsuite.directory.server;
 
 import com.powsybl.ws.commons.error.PowsyblWsProblemDetail;
@@ -9,6 +15,9 @@ import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * @author Mohamed Ben-rejeb {@literal <mohamed.ben-rejeb at rte-france.com>}
+ */
 class DirectoryExceptionTest {
 
     @Test
