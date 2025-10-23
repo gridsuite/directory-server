addc85a08309240cda703bb11060f14c14682902
benrejebmoh <mohamed.ben-rejeb@rte-france.com>
2025-10-23 15:23:32 +0200

    upgrade powsybl-ws-commons to 1.30.0 and use spring.application.name instead of server.name
    Signed-off-by: benrejebmoh <mohamed.ben-rejeb@rte-france.com>

--- src/main/java/org/gridsuite/directory/server/PropertyServerNameProvider.java
+++ src/main/java/org/gridsuite/directory/server/PropertyServerNameProvider.java
@@ -18,7 +18,7 @@ public class PropertyServerNameProvider implements ServerNameProvider {
 
     private final String name;
 
-    public PropertyServerNameProvider(@Value("${server.name:directory-server}") String name) {
+    public PropertyServerNameProvider(@Value("${spring.application.name:directory-server}") String name) {
         this.name = name;
     }
 
