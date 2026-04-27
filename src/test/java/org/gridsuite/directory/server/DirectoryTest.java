dbce77ad63163a872452144174097e1b121ebdb8
BOUHOURS Antoine <antoine.bouhours@rte-france.com>
2026-04-27 18:04:19 +0200

    New notifications on study creation

--- src/test/java/org/gridsuite/directory/server/DirectoryTest.java
+++ src/test/java/org/gridsuite/directory/server/DirectoryTest.java
@@ -67,8 +67,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.gridsuite.directory.server.NotificationService.HEADER_UPDATE_TYPE;
 import static org.gridsuite.directory.server.NotificationService.*;
 import static org.gridsuite.directory.server.dto.ElementAttributes.toElementAttributes;
-import static org.gridsuite.directory.server.services.ConsumerService.HEADER_STUDY_UUID;
-import static org.gridsuite.directory.server.services.ConsumerService.UPDATE_TYPE_STUDIES;
+import static org.gridsuite.directory.server.services.ConsumerService.*;
 import static org.gridsuite.directory.server.utils.DirectoryTestUtils.jsonResponse;
 import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -1286,7 +1285,40 @@ public class DirectoryTest {
     }
 
     @Test
-    public void testStudyUpdateNotification() throws Exception {
+    public void testStudyCreatedNotification() throws Exception {
+        String userId = "userId";
+
+        // Insert a root directory
+        ElementAttributes newRootDirectory = retrieveInsertAndCheckRootDirectory("newDir", userId);
+        UUID uuidNewRootDirectory = newRootDirectory.getElementUuid();
+
+        // Insert a study
+        UUID studyUuid = UUID.randomUUID();
+        String studyName = "studyName";
+        ElementAttributes subEltAttributes = toElementAttributes(studyUuid, studyName, TYPE_01, userId, "descr");
+        insertAndCheckSubElementInRootDir(uuidNewRootDirectory, subEltAttributes);
+
+        input.send(MessageBuilder.withPayload("")
+                .setHeader(HEADER_STUDY_UUID, studyUuid.toString())
+                .setHeader(HEADER_USER_ID, userId)
+                .setHeader(HEADER_UPDATE_TYPE, UPDATE_TYPE_STUDY_CREATED)
+                .build(), studyUpdateDestination);
+
+        // Assert that the broker message has been sent a directory update request message
+        Message<byte[]> message = output.receive(TIMEOUT, directoryUpdateDestination);
+        assertEquals("", new String(message.getPayload()));
+        MessageHeaders headers = message.getHeaders();
+        assertEquals(userId, headers.get(HEADER_USER_ID));
+        assertEquals(uuidNewRootDirectory, headers.get(HEADER_DIRECTORY_UUID));
+        assertEquals(true, headers.get(HEADER_IS_ROOT_DIRECTORY));
+        assertEquals(true, headers.get(HEADER_IS_PUBLIC_DIRECTORY));
+        assertEquals(NotificationType.UPDATE_DIRECTORY, headers.get(HEADER_NOTIFICATION_TYPE));
+        assertEquals(UPDATE_TYPE_DIRECTORIES, headers.get(HEADER_UPDATE_TYPE));
+        assertEquals(studyName, headers.get(HEADER_ELEMENT_NAME));
+    }
+
+    @Test
+    public void testStudyCreationFailedNotification() throws Exception {
         String userId = "userId";
 
         // Insert a root directory
@@ -1302,7 +1334,7 @@ public class DirectoryTest {
         input.send(MessageBuilder.withPayload("")
             .setHeader(HEADER_STUDY_UUID, studyUuid.toString())
             .setHeader(HEADER_USER_ID, userId)
-            .setHeader(HEADER_UPDATE_TYPE, UPDATE_TYPE_STUDIES)
+            .setHeader(HEADER_UPDATE_TYPE, UPDATE_TYPE_STUDY_CREATION_FAILED)
             .setHeader(HEADER_ERROR, "error")
             .build(), studyUpdateDestination);
 
