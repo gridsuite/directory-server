cd0a4a0cc5c7598a944aaddc8c1c45ac1f50faea
Florent MILLOT <florent.millot_externe@rte-france.com>
2026-07-27 13:05:12 +0200

    Revert "Handle null references when inserting an element"
    This reverts commit c3c22e837bb3796a837d5896054433e11763fde3.
    
    Signed-off-by: Florent MILLOT <florent.millot_externe@rte-france.com>

--- src/main/java/org/gridsuite/directory/server/DirectoryService.java
+++ src/main/java/org/gridsuite/directory/server/DirectoryService.java
@@ -158,8 +158,7 @@ public class DirectoryService {
             now,
             now,
             elementAttributes.getOwner(),
-            // references may be null when deserialized from a request that omits them (e.g. a plain element creation)
-            Optional.ofNullable(elementAttributes.getReferences()).orElseGet(List::of).stream().map(this::createReferenceEntity).toList());
+            elementAttributes.getReferences().stream().map(this::createReferenceEntity).toList());
 
         return tryInsertElement(elementEntity, parentDirectoryUuid, userId, generateNewName);
     }
